04f991e982a9cdb8c9fe152d3582820d7ccbf299
Luke Lalor <lukehlalor@gmail.com>
2024-08-29 17:59:12 -0700

    add passing test

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+import pytest
+from eidolon_ai_sdk.system.resources.resources_base import load_resources
+from eidolon_ai_sdk.test_utils.machine import TestMachine
+from eidolon_ai_sdk.test_utils.server import serve_thread
+from eidolon_ai_sdk.test_utils.vcr import vcr_patch
+
+
+@pytest.fixture(scope="module")
+def machine(tmp_path_factory):
+    return TestMachine(tmp_path_factory.mktemp("test_utils_storage"))
+
+
+@pytest.fixture(scope="module", autouse=True)
+def server(machine):
+    resources = load_resources([Path(__file__).parent.parent / "resources"])
+    with serve_thread([machine, *resources]):
+        yield
+
+
+@pytest.fixture(autouse=True)
+def state_manager(request, machine):
+    test_name = request.node.name
+    machine.reset_state()
+    with vcr_patch(test_name):
+        yield
+    machine.reset_state()
--- tests/test_agent_machine.py
+++ tests/test_agent_machine.py
@@ -1,2 +1,17 @@
+import pytest
+from eidolon_ai_client.client import Agent
+
+
+@pytest.fixture
+def agent():
+    return Agent.get("hello-world")
+
+
 def test_suite_initialization():
     assert True
+
+
+async def test_agent(agent: Agent):
+    process = await agent.create_process()
+    response = await process.action("converse", "Hi! What is the capital of France?")
+    assert "paris" in response.data.lower()
